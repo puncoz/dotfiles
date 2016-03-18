91322c32a3bb9557493924f2e606010386801a07
Luke Murphy <lukewm@riseup.net>
2016-03-18 19:35:03 +0100

    remove question clause

--- symlink.sh
+++ symlink.sh
@@ -2,26 +2,23 @@
 
 DOTDIR="$HOME/dotfiles/dotfiles"
 
-read -p "Symlink dotfiles? [yn] " answer
- if [[ $answer = y ]] ; then
-   mkdir -p ~/.irssi
-   cd $DOTDIR
+mkdir -p ~/.irssi
+cd $DOTDIR
 
-   # delete all dotfiles present, suppress stderr
-   for f in *; do
-     if [[ $f = "config" ]] ; then rm ~/.irssi/$f 2> /dev/null; fi
-     rm ~/.$f 2> /dev/null;
-   done
+# delete all dotfiles present, suppress stderr
+for f in *; do
+  if [[ $f = "config" ]] ; then rm ~/.irssi/$f 2> /dev/null; fi
+    rm ~/.$f 2> /dev/null;
+done
 
-   # symlink all dotfiles
-   for f in *; do
-     if [[ $f = "config" ]] ;
-       then
-         ln -s $(realpath ./$f) ~/.irssi/$f
-         file ~/.irssi/$f
-       else
-         ln -s $(realpath ./$f) ~/.$f
-         file ~/.$f
-     fi
-   done
- fi
+# symlink all dotfiles
+for f in *; do
+  if [[ $f = "config" ]] ;
+  then
+    ln -s $(realpath ./$f) ~/.irssi/$f
+    file ~/.irssi/$f
+  else
+    ln -s $(realpath ./$f) ~/.$f
+    file ~/.$f
+  fi
+done
