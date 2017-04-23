db8103a7ded923dc11580647d339e14eff11c0cc
Luke Murphy <lukewm@riseup.net>
2017-04-23 21:57:15 +0100

    Remove conditional and use more variables.

--- dotfiles/ipython/profile_lwm/startup/shortcuts.py
+++ dotfiles/ipython/profile_lwm/startup/shortcuts.py
@@ -5,23 +5,21 @@ from prompt_toolkit.keys import Keys
 
 
 def just_execute(event):
-    """https://github.com/ipython/ipython/issues/10405"""
     cbuffer = event.current_buffer
     if cbuffer.complete_state:
-        completion = cbuffer.complete_state.current_completion
+        state = cbuffer.complete_state
+        completion = state.current_completion
         if completion:
             cbuffer.apply_completion(completion)
         else:
             cbuffer.cancel_completion()
         return
-    cbuffer.accept_action.validate_and_handle(event.cli, cbuffer)
+    action = cbuffer.accept_action
+    action.validate_and_handle(event.cli, cbuffer)
 
 
-ip = get_ipython()
-if getattr(ip, 'pt_cli'):
-    registry = ip.pt_cli.application.key_bindings_registry
-    binding = registry.add_binding(
-        Keys.ControlJ,
-        filter=(HasFocus(DEFAULT_BUFFER) & ~HasSelection())
-    )
-    binding(just_execute)
+ip, key = get_ipython(), Keys.ControlJ
+focus, select = HasFocus(DEFAULT_BUFFER), HasSelection()
+registry = ip.pt_cli.application.key_bindings_registry
+binding = registry.add_binding(key, filter=(focus & ~select))
+binding(just_execute)
