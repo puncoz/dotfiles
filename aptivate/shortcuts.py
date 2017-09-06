84d88a8c181bedf62b0f534d6643ed3c9af261e6
Luke Murphy <lukewm@riseup.net>
2017-09-06 21:24:54 +0200

    Add place holder for Aptivate convenience stuff.

---
+++ aptivate/shortcuts.py
@@ -0,0 +1,6 @@
+"""Bits and bobs for getting around universe Aptivate quickly."""
+
+# TODO
+# An SSHer that knows the linode DNS and also knows environments
+# An accompanying script which exposes the functions
+# Have this automatically sourced from the .zshrc
---
+++ aptivate/shortcuts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# TODO
+# Wrappers for shortcut.py functions
