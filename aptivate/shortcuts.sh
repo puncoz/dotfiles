e3843e8499f1f9e30e07e81bbe5230f7866626a8
Luke Murphy <lukewm@riseup.net>
2017-09-07 10:39:11 +0200

    Add dodgy VPN quick fix SSH function.

--- aptivate/shortcuts.sh
+++ aptivate/shortcuts.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# TODO
-# Wrappers for shortcut.py functions
+function sshv(){
+  # An SSH that knows Aptivate DNS provider IPs
+  # FIXME: Dynamically configure the DNS when in VPN
+  IP=$(dig @ns1.linode.com "$1" +short)
+  ssh lukem@"$IP"
+}
